b493d1e38a92b789260df88121d56802af8f33e0
Otávio Melo <melo.otavio1212@gmail.com>
2024-07-08 20:52:23 -0300

    feat: refact header contact

--- src/components/ContactHeader.tsx
+++ src/components/ContactHeader.tsx
@@ -6,31 +6,25 @@ import { WhatsIcon } from './Icons';
 export const ContactHeader = () => {
   return (
     <div className="bg-teal-500 text-white  p-1">
-      <div className="container flex items-center justify-between">
-        <div className="flex gap-1 items-center">
+      <div className="container flex items-center justify-between gap-2 ">
+        <Link
+          className="flex items-center gap-1 text-xs md:text-sm hover:underline whitespace-nowrap"
+          href="https://wa.me/message/BQEMI636X4STE1"
+          rel="noreferrer noopener"
+          target="_blank"
+        >
           <WhatsIcon className="inline" />
-
-          <Link
-            className="text-sm hover:underline"
-            href="https://wa.me/message/BQEMI636X4STE1"
-            rel="noreferrer noopener"
-            target="_blank"
-          >
-            (35) 9 8466-3756
-          </Link>
-        </div>
-
-        <div>
-          <Link
-            className="flex items-center gap-1 hover:underline text-sm"
-            href="https://www.instagram.com/lumousautomacao/"
-            rel="noreferrer noopener"
-            target="_blank"
-          >
-            <InstagramIcon className="h-4 w-4" />
-            @lumousautomacao
-          </Link>
-        </div>
+          (35) 9 8466-3756
+        </Link>
+        <Link
+          className="flex items-center gap-1 hover:underline text-xs md:text-sm whitespace-nowrap"
+          href="https://www.instagram.com/lumousautomacao/"
+          rel="noreferrer noopener"
+          target="_blank"
+        >
+          <InstagramIcon className="h-4 w-4" />
+          @lumousautomacao
+        </Link>
       </div>
     </div>
   );
