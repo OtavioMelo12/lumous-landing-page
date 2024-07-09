dc54f82523be8de9b47fdb506bd016b4d11dbfb8
Otávio Melo <melo.otavio1212@gmail.com>
2024-07-09 20:54:17 -0300

    feat: add gtm

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 
 import { Montserrat } from 'next/font/google';
+import Script from 'next/script';
 
 import { ContactHeader } from '@/components/ContactHeader';
 import { Footer } from '@/components/Footer';
@@ -63,6 +64,16 @@ export default function RootLayout({
   return (
     <html lang="pt-Br">
       <head>
+        <Script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-NZVLR273');`,
+          }}
+          id="gtm-script"
+        />
         <meta
           content="F4DUd3kmkIwd-MCuSPKfPpDebCrPG2f84Fx8ADuLwZ4"
           name="google-site-verification"
@@ -74,6 +85,18 @@ export default function RootLayout({
           montserrat.className,
         )}
       >
+        <noscript>
+          <iframe
+            height="0"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-N36SPKLD"
+            style={{
+              display: 'none',
+              visibility: 'hidden',
+            }}
+            title="gtm"
+            width="0"
+          />
+        </noscript>
         <ThemeProvider
           attribute="class"
           defaultTheme="light"
