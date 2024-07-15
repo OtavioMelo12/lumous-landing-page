75086f90206a79564e247d6b0722d10d0eb550bc
Otávio Melo <melo.otavio1212@gmail.com>
2024-07-15 14:05:52 -0300

    feat: google tag

--- src/components/google-tag-manager/GoogleTagManager.tsx
+++ src/components/google-tag-manager/GoogleTagManager.tsx
@@ -7,11 +7,7 @@ interface GoogleTagManagerProps {
 
 const GoogleTagManager = ({ gtmId }: GoogleTagManagerProps) => {
   React.useEffect(() => {
-    if (process.env.NEXT_PUBLIC_APP_ENV !== 'production') {
-      return console.log('🚫 => NAO INICIANDO GOOGLE TAG MANAGER');
-    } else {
-      TagManager.initialize({ gtmId });
-    }
+    TagManager.initialize({ gtmId });
   }, [gtmId]);
 
   return null;
